035565432d68843ccb4231bdc6a132751db47851
Nader <nelgh048@uottawa.ca>
2019-07-31 21:39:39 -0400

    added hashmap

--- Keys.cpp
+++ Keys.cpp
@@ -1,3 +1,22 @@
-class Keys {
+#include "sha256.h"
+#include <unordered_map>
+using namespace std;
+using std::string;
 
+class Keys {
+  private:
+    unordered_map<string, string> keyPairs;
+
+    void setMap() {
+      for(int i = 1; i<10000; i++) {
+        string privateKey = std::to_string(i);
+        string publicKey = sha256(std::to_string(i));
+        keyPairs[privateKey] = publicKey;
+      }
+    }
+
+  public:
+    Keys() {
+      setMap();
+    }
 }
